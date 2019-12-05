429c60cea418c0b88c49a37f573a8caafae9fdd1
Matt Malone <mattm@apadmi.com>
2019-12-05 23:11:33 +0000

    Way more difficult than it needed to be

--- day5/day5p2.py
+++ day5/day5p2.py
@@ -37,7 +37,11 @@ def get_value (input, index, mode):
 def get_dest (input, index, mode):
     return index
 
+# 999 - <8
+# 1000 - =8
+# 1001 - >8
 instructions = []
+
 
 with open('input.txt', 'r') as file:
     data = file.read()
@@ -47,9 +51,13 @@ with open('input.txt', 'r') as file:
 # 2 - Multiply two integers together, store in 3rd
 # 3 - Store input at address
 # 4 - Output value at address
+# 5 - Jump to second param if first true
+# 6 - Jump to second param if first false
+# 7 - Less Than (1st < 2nd), set position 3 to 1 or 0
+# 8 - Equals (1st == 2nd), set position 3 to 1 or 0
 # 99 - End
 
-input = 1
+input = 5
 output = 0
 
 idx = 0
@@ -63,7 +71,7 @@ print("Have instructions", len(instructions))
 
 while idx < len(instructions):
     raw = instructions[idx]
-    print("Raw {}".format(raw))
+    print("Index: {}, Raw: {}".format(idx, raw))
 
     operation = make_operation(raw)
 
@@ -74,8 +82,7 @@ while idx < len(instructions):
 
     if val == 1:
         sl = instructions[idx + 1: idx + 4]
-        print("Add slice {}".format(sl))
-        # print("Slice",  sl)
+        print("Slice",  sl)
         # print(instructions[idx:idx+10])
         p1 = get_value(instructions, sl[0], param1Mode)
         p2 = get_value(instructions, sl[1], param2Mode)
@@ -83,6 +90,7 @@ while idx < len(instructions):
         # dest = sl[2]
         dest = get_dest(instructions, sl[2], param3Mode)
         instructions[dest] = newval
+        print("Adding - {} to {}".format(newval, dest))
         idx += 4
     elif val == 2:
         sl = instructions[idx + 1: idx + 4]
@@ -93,18 +101,55 @@ while idx < len(instructions):
         # dest = sl[2]
         dest = get_dest(instructions, sl[2], param3Mode)
         instructions[dest] = newval
+        print("Mul - {} to {}".format(newval, dest))
+
         idx += 4
     elif val == 3:
-        dest = get_dest(instructions, instructions[idx+1], param1Mode)
+        # dest = get_dest(instructions, instructions[idx+1], param1Mode)
+        dest = instructions[idx+1]
         instructions[dest] = input
         print("Input set {} to value {}".format(dest, input))
         idx += 2
     elif val == 4:
-        val = instructions[idx+1] if param1Mode else instructions[instructions[idx+1]]
-        output = val
+        output = get_value(instructions, instructions[idx+1], param1Mode)
         print("Output set {} to value {}".format(dest, output))
-
         idx += 2
+    elif val == 5:
+        sl = instructions[idx + 1: idx + 3]
+        print("Jump true slice {}".format(sl))
+        p1 = get_value(instructions, sl[0], param1Mode)
+        dest = get_value(instructions, sl[1], param2Mode)
+        if p1 != 0:
+            print("Jumping to", dest)
+            idx = dest
+        else:
+            idx += 3
+    elif val == 6:
+        sl = instructions[idx + 1: idx + 3]
+        print("Jump false slice {}".format(sl))
+        p1 = get_value(instructions, sl[0], param1Mode)
+        dest = get_value(instructions, sl[1], param2Mode)
+        if p1 == 0:
+            print("Jumping to", dest)
+            idx = dest
+        else:
+            idx += 3
+    elif val == 7:
+        sl = instructions[idx + 1: idx + 4]
+        print("< slice {}".format(sl))
+        p1 = get_value(instructions, sl[0], param1Mode)
+        p2 = get_value(instructions, sl[1], param2Mode)
+        dest = get_dest(instructions, sl[2], param3Mode)
+        instructions[dest] = 1 if p1 < p2 else 0
+        idx += 4
+    elif val == 8:
+        sl = instructions[idx + 1: idx + 4]
+        print("= slice {}".format(sl))
+        p1 = get_value(instructions, sl[0], param1Mode)
+        p2 = get_value(instructions, sl[1], param2Mode)
+        dest = get_dest(instructions, sl[2], param3Mode)
+        instructions[dest] = 1 if p1 == p2 else 0
+        idx += 4
     else:
         print("Breaking at val {}".format(val))
         break
