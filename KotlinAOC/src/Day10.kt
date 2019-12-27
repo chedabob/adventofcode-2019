95edb10973b317a68a30eb667b570bb864fad0a8
Matt Malone <mattm@apadmi.com>
2019-12-27 10:22:56 +0000

    Day 10

--- KotlinAOC/src/Day10.kt
+++ KotlinAOC/src/Day10.kt
@@ -1,5 +1,4 @@
-import kotlin.math.atan2
-import kotlin.math.sqrt
+import kotlin.math.*
 
 fun main() {
     val raw = "#...##.####.#.......#.##..##.#." +
@@ -35,7 +34,7 @@ fun main() {
             "#..###.....##.#.......#.##...##"
 
     val width = sqrt(raw.length.toDouble()).toInt()
-    println("Width $width")
+//    println("Width $width")
     val coords = (raw.indices).map { idxToCoord(it, width) }
 //    println (coords)
 
@@ -45,18 +44,42 @@ fun main() {
     val asteroids = pairs.filter { it.first == '#' }.map { it.second }
 
     val collisions = asteroids.map { asteroidRays(it, asteroids) }
-    val max = collisions.maxBy { it.size }
+    val max = collisions.maxBy { it.second.size }
 
-    println("Maximum : ${max?.size}")
+    println("Day 10 Part 1 - Maximum : ${max?.second?.size}")
+
+
+    vaporise(max!!.first, asteroids)
+
 }
+class Target(val location: Pair<Int, Int>, val distance: Double, val angle: Double)
 
-fun asteroidRays(a : Pair<Int, Int>, others: List<Pair<Int, Int>>): Set<Double> {
+fun asteroidRays(a : Pair<Int, Int>, others: List<Pair<Int, Int>>): Pair<Pair<Int,Int>,Set<Double>> {
     val notMe = others.filterNot { it == a }
-    return notMe.map { arcTan(a, it) }.toSet()
+    return Pair(a, notMe.map { arcTan(a, it) }.toSet())
 }
+
+fun asteroidRays2(a : Pair<Int, Int>, others: List<Pair<Int, Int>>): List<Target> {
+    val angleDistance = others.map { Target(it, distance(a, it), arcTan2(a, it)) }
+    return angleDistance.groupBy { q -> q.angle }.map { t -> t.value.minBy { it.distance }!! }.sortedBy { it.angle }.reversed()
+}
+
+fun distance (a : Pair<Int, Int>, b: Pair<Int,Int>) : Double {
+    val x = (a.first - b.first).toDouble().pow(2.0)
+    val y = (a.second - b.second).toDouble().pow(2.0)
+
+    return sqrt(x + y)
+}
+
 
 fun arcTan (a : Pair<Int, Int>, b : Pair<Int, Int>) : Double {
-    return atan2((a.first - b.first).toDouble(), (a.second - b.second).toDouble())
+    return atan2((b.first - a.first).toDouble(), (b.second - a.second).toDouble())
+}
+
+
+fun arcTan2 (a : Pair<Int, Int>, b : Pair<Int, Int>) : Double {
+    val angle = atan2((a.first - b.first).toDouble(), (a.second - b.second).toDouble())
+    return if (angle > 0) angle - (2 * PI) else angle
 }
 
 fun idxToCoord (idx : Int, width : Int) : Pair<Int, Int> {
@@ -64,4 +87,32 @@ fun idxToCoord (idx : Int, width : Int) : Pair<Int, Int> {
     val rows = idx - (columns * width)
 
     return Pair(rows, columns)
+}
+
+fun vaporise (monitoringLocation : Pair<Int, Int>, asteroids: List<Pair<Int, Int>>) {
+    var toCheck = asteroids.filterNot { it == monitoringLocation }
+
+    var numVaporised = 1
+
+    while (true) {
+        var thisPass = asteroidRays2(monitoringLocation, toCheck)
+        while (thisPass.isNotEmpty()) {
+            val next = thisPass.first()
+            if (numVaporised == 200) {
+                println("Day 10 Part 2 ${calcFinal(next.location)}")
+                return
+            }
+            else {
+                thisPass = thisPass.takeLast(thisPass.size - 1)
+                toCheck = toCheck.filterNot { it == next.location }
+                numVaporised++
+            }
+        }
+
+    }
+
+}
+
+fun calcFinal (input : Pair<Int, Int>): Int {
+    return (input.first * 100) + input.second
 }
