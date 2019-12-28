9202288e3909ebafb8d0e982d1086d593f2649b4
Matt Malone <mattm@apadmi.com>
2019-12-28 18:55:36 +0000

    Day10 fragments

--- day10/day10.hs
+++ day10/day10.hs
@@ -1,1 +1,14 @@
-main = putStrLn "Hello, World!"
+import qualified Data.Text as T
+
+main :: IO ()
+main = do
+    s <- readFile "test.txt"
+    let width = 5
+    let removeLines xs = [ x | x <- xs, not (x `elem` "\n") ]
+    let getindex :: Integer -> Integer -> Integer
+    let getindex x y = (+ x (* y width))
+    -- let getchar xs x y = xs !! (+ x (* y width))
+    let lines =  removeLines s
+    -- let iterations = [ [x,y] | x <- [0..10], y <- [0..10]]
+    -- let pairs = [ [x, y] | x <- iterations, y <- lines !! (head x)]
+    print $ (getindex 1, 0)
